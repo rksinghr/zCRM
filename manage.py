b266cd6746d5564a020d8c236998dc4639054c24
Ritesh Singh <40223237+rksinghr@users.noreply.github.com>
2024-05-01 18:04:09 +0530

    Updated wsgi and manage.py

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     settings_module = 'zCRM.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'zCRM.settings'
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings_module')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
